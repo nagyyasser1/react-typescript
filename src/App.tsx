8c0d40a8f4397fa51141e0784654f345d207d810
nagy <nagyy8751@gmail.com>
2023-12-24 17:01:35 +0200

    v1

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { Container } from "react-bootstrap";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Store } from "./pages/Store";
+import { About } from "./pages/About";
+import { Navbar } from "./components/Navbar";
+import { ShoppingCartProvider } from "./context/shoppingCartContext";
+
+function App() {
+  return (
+    <ShoppingCartProvider>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+    </ShoppingCartProvider>
+  );
+}
+
+export default App;
